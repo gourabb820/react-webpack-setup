c4d24d27d024c8b1524dd92c1abc8765e3979726
Gourab Banerjee <gourabb68@gmail.com>
2021-11-16 12:12:20 +0530

    comp added

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
-console.log("It works!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = ()=>{
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+
+ReactDOM.render(<App />,document.getElementById("root"))
