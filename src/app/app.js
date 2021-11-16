217753d81384935c8e828898308fe77cd53fd50f
Gourab Banerjee <gourabb68@gmail.com>
2021-11-16 18:21:40 +0530

    using hooks useContext

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React ,{useContext} from "react";
 import ReactDOM from "react-dom";
 import myContext from "./context";
+
+// const App = () => {
+//   return (
+//     <myContext.Consumer>
+//       {(content) => <h1>Hello World {content.name}</h1>}
+//     </myContext.Consumer>
+//   );
+// };
+// export default App;
+
+
+
+//using hooks
+
 const App = () => {
+  const {name} = useContext(myContext)
   return (
-    <myContext.Consumer>
-      {(content) => <h1>Hello World {content.name}</h1>}
-    </myContext.Consumer>
+    <h1>Hello World {name}</h1>
   );
 };
 export default App;
